ad66ab852db6b782fbf60e17bcc68adc198b6d3b
Shiv Deepak Muddada <shiv@aeroh.org>
2022-04-07 09:18:19 -0700

    provide app info link in manual permission activity

--- app/src/main/java/io/aeroh/android/SetManualPermissionActivity.java
+++ app/src/main/java/io/aeroh/android/SetManualPermissionActivity.java
@@ -3,7 +3,9 @@ package io.aeroh.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,5 +27,17 @@ public class SetManualPermissionActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        Button btnAppSettings = findViewById(R.id.btnAppSettings);
+        btnAppSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+
     }
 }
