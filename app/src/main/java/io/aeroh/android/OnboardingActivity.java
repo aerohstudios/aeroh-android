5be99901763f077814e3aa7f0eda56d406a9c520
Ayush Shrivastav <ayush@aeroh.org>
2023-07-02 23:11:44 +0530

    Implemented Onboarding Activity with navigation

---
+++ app/src/main/java/io/aeroh/android/OnboardingActivity.java
@@ -0,0 +1,43 @@
+package io.aeroh.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    Button sign_up_button, login_button;
+    int delay = 500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+
+        sign_up_button = findViewById(R.id.signupbtn);
+        login_button = findViewById(R.id.loginbtn);
+
+        sign_up_button.setOnClickListener(v -> {
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }, delay);
+        });
+
+
+        login_button.setOnClickListener(v -> {
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }, delay);
+
+        });
+}}
